27681098c6980fa871030fb955e37589c610a64d
sanjitkapoor <88198415+sanjitkapoor@users.noreply.github.com>
2021-11-15 02:51:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,149 @@
+import React, { useRef, useState, useEffect } from "react";
+///////// NEW STUFF ADDED USE STATE
+
+// import logo from './logo.svg';
+import * as tf from "@tensorflow/tfjs";
+import * as handpose from "@tensorflow-models/handpose";
+import Webcam from "react-webcam";
+import "./App.css";
+import { drawHand } from "./utilities";
+
+///////// NEW STUFF IMPORTS
+import * as fp from "fingerpose";
+//import victory from "./victory.png";
+//import thumbs_up from "./thumbs_up.png";
+///////// NEW STUFF IMPORTS
+
+function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  ///////// NEW STUFF ADDED STATE HOOK
+  const [emoji, setEmoji] = useState(null);
+  const images = { thumbs_up: "Thumbs up", victory: "Victory" };
+  const colors = { thumbs_up: "green", victory: "red" };
+  let color = "";
+  ///////// NEW STUFF ADDED STATE HOOK
+
+  const runHandpose = async () => {
+    const net = await handpose.load();
+    console.log("Handpose model loaded.");
+    //  Loop and detect hands
+    setInterval(() => {
+      detect(net);
+    }, 1000);
+  };
+
+  const detect = async (net) => {
+    // Check data is available
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas height and width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // Make Detections
+      const hand = await net.estimateHands(video);
+      // console.log(hand);
+
+      ///////// NEW STUFF ADDED GESTURE HANDLING
+
+      if (hand.length > 0) {
+        const GE = new fp.GestureEstimator([
+          fp.Gestures.VictoryGesture,
+          fp.Gestures.ThumbsUpGesture,
+        ]);
+        const gesture = await GE.estimate(hand[0].landmarks, 9);
+        if (gesture.gestures !== undefined && gesture.gestures.length > 0) {
+
+          const confidence = gesture.gestures.map(
+            (prediction) => prediction.score
+          );
+
+          const maxConfidence = confidence.indexOf(
+            Math.max.apply(null, confidence)
+          );
+
+          if(maxConfidence != -1){
+            console.log(gesture.gestures[maxConfidence].name);
+            setEmoji(gesture.gestures[maxConfidence].name);
+            color = colors[emoji];
+            
+          }
+        }
+      }
+      const ctx = canvasRef.current.getContext("2d");
+      drawHand(hand, ctx);
+    }
+  };
+
+  useEffect(()=>{runHandpose()},[]);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "30px",
+            left: 0,
+            right: 0,
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "30px",
+            left: 0,
+            right: 0,
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+        {/* NEW STUFF */}
+        {emoji !== null ? (
+          <div
+            style={{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "30px",
+              left: 0,
+              bottom: 0,
+              right: 0,
+              textAlign: "center",
+              height: 640,
+              width: 480,
+              backgroundColor: {color},
+            }}
+
+          >{images[emoji]}</div>
+
+        ) : (
+          ""
+        )}
+
+        {/* NEW STUFF */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
